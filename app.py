d47adaf8e6eb8e420f88378906fd9a4d7adb30f5
Lakshmi Tulasi Mandala <lakshmitulasimandala@gmail.com>
2025-10-23 01:34:08 +0530

    Added transaction form with note field and history page with tables and totals

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect
+import sqlite3
+from datetime import date 
+from flask import Flask, render_template, redirect,request
 
 app = Flask(__name__)
 
@@ -6,13 +8,43 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/add', methods = ["POST"])
+
+
+@app.route('/add_transaction', methods = ["POST"])
 def add_transaction():
-    return redirect("/")
+    amount = float(request.form['amount'])
+    category = request.form['category']
+    note = request.form.get('note','')
+    today = date.today().strftime("%Y-%m-%d")
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO transactions (date, category, amount, note) VALUES (?, ?, ?, ?)''',
+        (today, category, amount, note)
+    )
+    
+    conn.commit()
+    conn.close()
+
+
+    return redirect("/history")
+
+
 
 @app.route('/history')
 def history():
-    return render_template("history.html")
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM transactions ORDER by id DESC")
+    transactions = cursor.fetchall()
+
+    total_amount = sum([t[3] for t in transactions])  # t[3] is the 'amount' column
+
+    conn.close()
+    return render_template('history.html',transactions = transactions, total_amount= total_amount)
 
 if __name__ == "__main__":
     app.run(debug=True)
